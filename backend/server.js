5c0b994831a1c747d785a58c12bdc5ecfcde0456
gowdas1997 <gowdaskr1997@gmail.com>
2026-03-22 22:53:22 +0530

    new change in server file

--- backend/server.js
+++ backend/server.js
@@ -37,6 +37,6 @@ app.get('/', (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Capstone server running on http://localhost:${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Capstone server running on port ${PORT}`);
 });
